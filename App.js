7e3ae16e893280089672766a46d5f86e8d878cf2
Furkan Doğu <furkandogu2018@gmail.com>
2024-05-02 19:48:53 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native';
+import GameStartScreen from './screens/GameStartScreen';
+
+export default function App() {
+  return (
+    <GameStartScreen />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
